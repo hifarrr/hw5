e648b2d89e357d89c4438e707de69e84939ac596
yyyyyuan <31830607+yyyyyuan@users.noreply.github.com>
2017-10-10 20:40:46 -0700

    Create lewitt-725.js

---
+++ lewitt-725.js
@@ -0,0 +1,13 @@
+function setup() { 
+	createCanvas(400, 400);
+	colorMode(RGB); 
+	background(92,166,214);
+}
+
+function draw(){
+	fill(0);
+	stroke(255);
+	strokeWeight(10);
+	rectMode(CENTER);
+	rect(200,200,200,200);
+}
