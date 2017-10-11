365aaf24055cc11873abb94c91fca386d4bd1c79
yyyyyuan <31830607+yyyyyuan@users.noreply.github.com>
2017-10-10 18:17:03 -0700

    Create sketch-3.js

---
+++ sketch-3.js
@@ -0,0 +1,57 @@
+var circle = {
+  x: 100, 
+  y: 100,
+  vx: 3,
+  vy: -4,
+  r: 10,
+	a: 300
+};
+
+var circle2 = {
+  x: 120, 
+  y: 130,
+  vx: 3,
+  vy: -4,
+  r: 10,
+	a: 200
+};
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+  
+  paint(circle);
+  paint(circle2);
+	
+  move(circle);
+  move(circle2);
+	
+  bounce(circle);
+  bounce(circle2);
+}
+
+function paint(circle) {
+	noStroke();
+	colorMode(HSB);
+	fill(circle.a,20,100);
+  ellipse(circle.x, circle.y, circle.r*2, circle.r*2);
+}
+
+function move(circle) {
+  circle.x += circle.vx; // circle.x = circle.x + circle.vx
+  circle.y += circle.vy;
+}
+
+
+function bounce(circle) {
+  if (circle.x > width || circle.x < 0){
+    circle.vx = - circle.vx;
+  }
+  if (circle.y<0) {
+   circle.y=height
+	}
+}
