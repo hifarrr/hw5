4d0af4c4659da441fc87e1da9e9506d3d1ae3c9a
yyyyyuan <31830607+yyyyyuan@users.noreply.github.com>
2017-10-14 18:33:48 -0700

    Create array-4.js

---
+++ array-4.js
@@ -0,0 +1,14 @@
+var shades =[10,20,45,25,60,85,100,150,200];
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  colorMode(HSB);
+  noStroke();
+	for(var i=0; i<shades.length;i++){
+  	fill(shades[i],100,100);
+		rect(i*width/shades.length,0,width/shades.length,height);
+	}
+}
